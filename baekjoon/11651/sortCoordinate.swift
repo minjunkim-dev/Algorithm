449fcf834456993cd2ffb7e02155579cc5dcd6f7
minjunkim.dev <minjunkim.dev@gmail.com>
2022-05-02 22:40:48 +0900

    좌표 정렬하기 2: https://www.acmicpc.net/problem/11651 정렬 기준이 여러개라면 우선순위가 낮은 정렬 기준으로 순서대로 정렬하면 됨

---
+++ baekjoon/11651/sortCoordinate.swift
@@ -0,0 +1,38 @@
+//좌표 정렬하기 2: https://www.acmicpc.net/problem/11651 정렬 기준이 여러개라면 우선순위가 낮은 정렬 기준으로 순서대로 정렬하면 됨
+
+import Foundation
+
+struct Coordinate {
+    let x: Int
+    let y: Int
+    
+    init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+    
+    func printCoordinate() {
+        print(self.x, self.y)
+    }
+}
+
+let iteration = Int(readLine()!)!
+var coordinates: [Coordinate] = []
+
+for _ in 0..<iteration {
+    let input = readLine()!
+        .split(separator: " ")
+        .map { Int($0)! }
+    let (x, y) = (input[0], input[1])
+    let pos = Coordinate(x: x, y: y)
+    coordinates.append(pos)
+}
+
+coordinates.sorted {
+    $0.x < $1.x
+}.sorted {
+    $0.y < $1.y
+}.forEach {
+    $0.printCoordinate()
+}
+
